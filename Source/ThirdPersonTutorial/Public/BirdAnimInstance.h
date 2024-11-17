3035dfab97509fd1e21b1d5cbdbea0a32e74d732
MuhammadMomin504 <momin.1867@gmail.com>
2024-11-17 15:20:47 +0500

    add BirdAnimInstance C++ class

---
+++ Source/ThirdPersonTutorial/Public/BirdAnimInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "BirdAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THIRDPERSONTUTORIAL_API UBirdAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+};
